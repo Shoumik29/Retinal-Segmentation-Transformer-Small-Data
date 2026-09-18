e2c5cd44984c72bfe9040a3e509236aa2e9c788e
Rudro <rudrosaha103863@gmail.com>
2026-09-19 01:29:32 +0600

    preprocessing is added

---
+++ src/retina_seg/data/preprocessing.py
@@ -0,0 +1,27 @@
+"""
+Preprocessing utilities for retinal fundus image segmentation.
+
+Responsibilities:
+    - Enhance fundus images (CLAHE, gamma, LAB, channel fusion, PCA, N4 bias correction)
+    - Detect the circular field of view and neutralise the black border ring
+"""
+
+import os
+from glob import glob
+from typing import Callable, Dict, List, Tuple
+
+import cv2
+import numpy as np
+import SimpleITK as sitk
+import tensorflow as tf
+from scipy.io import loadmat
+from albumentations import (
+    Compose,
+    HorizontalFlip,
+    HueSaturationValue,
+    RandomBrightnessContrast,
+    Rotate,
+    VerticalFlip,
+)
+from sklearn.decomposition import PCA
+
